32590b5c15008af46dda5cd121869a35d2ba8f7b
peterhpchen <peterhsinpingchen@gmail.com>
2021-11-08 22:29:54 +0800

    refactor: use async func

--- app.js
+++ app.js
@@ -4,16 +4,12 @@ const https = require("https");
 const { env } = require("process");
 const { clientId, clientSecret } = env;
 
-const server = http.createServer((req, res) => {
-  const { code } = url.parse(req.url, true).query;
-  res.writeHead(200, { "Content-Type": "text/html" });
+const getReqGitHubIdentityUrl = (clientId) =>
+  `https://github.com/login/oauth/authorize?client_id=${clientId}`;
 
-  if (!code) {
-    res.end(
-      `<html><body><a href="https://github.com/login/oauth/authorize?client_id=${clientId}">login</a></body></html>`
-    );
-  } else {
-    const tokenReq = https.request(
+const exchangeCodeForToken = async (clientId, clientSecret, code) =>
+  new Promise((resolve) => {
+    const req = https.request(
       "https://github.com/login/oauth/access_token",
       {
         method: "POST",
@@ -23,54 +19,72 @@ const server = http.createServer((req, res) => {
         },
       },
       (tokenRes) => {
-        const tokenBody = [];
+        const body = [];
         tokenRes.on("data", (chunk) => {
-          tokenBody.push(chunk);
+          body.push(chunk);
         });
         tokenRes.on("end", () => {
-          const { access_token } = JSON.parse(
-            Buffer.concat(tokenBody).toString()
-          );
-          console.log(access_token);
-
-          const userReq = https.get(
-            "https://api.github.com/user",
-            {
-              headers: {
-                Accept: "application/vnd.github.v3+json",
-                Authorization: `token ${access_token}`,
-                "User-Agent": "peter-test",
-              },
-            },
-            (userRes) => {
-              const userBody = [];
-
-              userRes.on("data", (chunk) => {
-                userBody.push(chunk);
-              });
-              userRes.on("end", () => {
-                // const userData = JSON.parse(Buffer.concat(userBody).toString());
-                const { name } = JSON.parse(Buffer.concat(userBody).toString());
-                console.log(name);
-
-                res.end(`<html><body>Hello, ${name}!</body></html>`);
-              });
-            }
-          );
-          console.log(userReq.getHeader("Authorization"));
-          userReq.end();
+          const { access_token } = JSON.parse(Buffer.concat(body).toString());
+          resolve(access_token);
         });
       }
     );
-    tokenReq.write(
+    req.write(
       JSON.stringify({
         client_id: clientId,
         client_secret: clientSecret,
         code,
       })
     );
-    tokenReq.end();
-  }
-});
+    req.end();
+  });
 
-server.listen(3000);
+const useTokenAccessAPI = async (access_token) =>
+  new Promise((resolve) => {
+    const req = https.get(
+      "https://api.github.com/user",
+      {
+        headers: {
+          // https://docs.github.com/en/rest/overview/resources-in-the-rest-api#current-version
+          Accept: "application/vnd.github.v3+json",
+          // https://docs.github.com/en/rest/overview/resources-in-the-rest-api#authentication
+          Authorization: `token ${access_token}`,
+          // https://docs.github.com/en/rest/overview/resources-in-the-rest-api#user-agent-required
+          // https://stackoverflow.com/a/21384582/9265131
+          "User-Agent": "github-oauth-demo",
+        },
+      },
+      (userRes) => {
+        const body = [];
+
+        userRes.on("data", (chunk) => {
+          body.push(chunk);
+        });
+        userRes.on("end", () => {
+          const { name } = JSON.parse(Buffer.concat(body).toString());
+          resolve(name);
+        });
+      }
+    );
+    req.end();
+  });
+
+http
+  .createServer(async (req, res) => {
+    const { code } = url.parse(req.url, true).query;
+    res.writeHead(200, { "Content-Type": "text/html" });
+
+    if (!code) {
+      const href = getReqGitHubIdentityUrl(clientId);
+      res.end(`<html><body><a href="${href}">login</a></body></html>`);
+    } else {
+      const access_token = await exchangeCodeForToken(
+        clientId,
+        clientSecret,
+        code
+      );
+      const name = await useTokenAccessAPI(access_token);
+      res.end(`<html><body>Hello, ${name}!</body></html>`);
+    }
+  })
+  .listen(3000);
